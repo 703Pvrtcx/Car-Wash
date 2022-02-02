16b3652080981d1e108960af7bdb790d0902301a
703Pvrtcx <kasaokapapkies@gmail.com>
2022-02-02 12:49:13 +0200

    Added A new image from both local and url

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+// import logo from './assets/img0.png'; 
+import logo from './assets/img0.png'; 
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Welcome to my First React Native Application</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+      <Image source={logo} style={{ width: 305, height: 159 }} /> 
+      {/* <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={{ width: 305, height: 159 }} /> */}
+    
+      <Text style={{color: '#888', fontSize: 18}}>
+          just press the button below!
+      </Text>
+        <StatusBar style="auto" />
+     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
